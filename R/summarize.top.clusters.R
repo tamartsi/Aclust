e39327db8ff4f6455f4b5a3f6e36bf98b0a14937
tamartsi <tamartsi@gmail.com>
2015-01-05 21:20:56 -0800

    Added the option to print progress messages
    Set print.progress = TRUE to see messages providing the function’s
    progress (what it is working on currently). The function will also
    print parts of intermediate data (potentially created by the function,
    to allow for convenient debugging.

--- R/summarize.top.clusters.R
+++ R/summarize.top.clusters.R
@@ -1,26 +1,14 @@
 summarize.top.clusters <-
-function(betas, covariates, exposure, id, 
-         clusters.GEE.results = NULL, 
-         clusters.GEE.results.file = NULL, 
-         minimum.sites = 2, top.number = 10, 
-         cutoff.fdr.pval = 0.05, cutoff.effect.size = NULL, 
-         annot = NULL, annotation.file.name = NULL, 
-         required.annotation = c("IlmnID", "Coordinate_37", 
-                                 "UCSC_RefGene_Name",
-                                 "UCSC_RefGene_Group", 
-                                 "UCSC_CpG_Islands_Name", 
-                                 "Relation_to_UCSC_CpG_Island"), 
-         file.to.print.report = NULL){
+function(betas, covariates, exposure, id, clusters.GEE.results = NULL, clusters.GEE.results.file = NULL, minimum.sites = 2, top.number = 10, cutoff.fdr.pval = 0.05, cutoff.effect.size = NULL, annot = NULL, annotation.file.name = NULL, required.annotation = c("IlmnID", "Coordinate_36", "UCSC_RefGene_Name","UCSC_RefGene_Group", "UCSC_CpG_Islands_Name", "Relation_to_UCSC_CpG_Island"), file.to.print.report = NULL, print.progress = F){
 ## Returns a summary of the top clusters:
-## Chooses the top clusters according to the top.number provided, 
-## with FDR p-value at most 0.05, and with effect size at least cutoff.effect.size (if given)
+## Chooses the top clusters according to the top.number provvided, with FDR p-value at most 0.05, and with effect size at least cutoff.effect.size (if given)
 ## For each of the top cluster, returns exposure effect size and pvalue before and after FDR, and illumina annotation
-## Also returns individual GEE analysis of sites in the cluster (i.e. with sandwich standard error)
+## Also returns in dividual GEE analysis of sites in the cluster (i.e. with sandwich standard error)
 ## Finally, the returnd lists are ordered by exposure effect size on the cluster
-## If a file to print report is given, then a latex file is generated with two tables. 
-## One table summarized the annotation and effects of
+## If a file to print report is given, then a latex file is generated with two tables. One table summarized the annotation and effects of
 ## the top clusters, annother table summarizes the individual sites analysis of the sites from the top clusters. 
 ## minimum.sites is the minimal size of clusters to consider. The FDR correction is applied only these clusters. 
+	require(data.table)
 	 
 	if (!is.null(clusters.GEE.results)) anal.results <- clusters.GEE.results 
 		else anal.results <- read.table(clusters.GEE.results.file, header = T, as.is = T)
@@ -44,14 +32,19 @@ function(betas, covariates, exposure, id,
 	
 	top.number <- min(top.number, nrow(anal.results))	
 	
+	if (print.progress) message(paste("up to ", top.number, "of clusters will be analyzed and reported"))
+	
 	if (top.number == 0){
 		cat("0 clusters were found with corrected p-value smaller than ",  cutoff.fdr.pval, "no output produced" ,"/n")
 	}  else {
 	
+	
+	
 	last.one <- nrow(anal.results)	
 	top.clusters <- anal.results[last.one:(last.one - top.number + 1)]
 	
-	### Top.clusters will be output
+	### Top.clusters will be outputed 
+	
 	
 	sites.vec <- rep("", length = sum(top.clusters$n_sites_in_cluster))
 	cluster.vec <- rep(0, sum(top.clusters$n_sites_in_cluster))
@@ -63,25 +56,38 @@ function(betas, covariates, exposure, id,
 		cluster.vec[ind:(ind + top.clusters$n_sites_in_cluster[i] -1)] <- i
 
 		ind <- ind + top.clusters$n_sites_in_cluster[i]
+		
+		if (print.progress) message(paste("The ", i, "-th cluster sites were parsed"))
+		
 	}
 	
 	annotation.top.clusters <- annot.probe.vec(sites.vec, annot = annot, annotation.file.name = annotation.file.name, required.annotation = required.annotation)
+	
+	if (print.progress){ 
+		message(paste("This is the vector with cluster assignments of the top clusters, of length", length(cluster.vec)))
+		print(cluster.vec)
+		
+		message(paste("This is the annotation of top clusters, describing", nrow(annotation.top.clusters), "sites"))
+		print(annotation.top.clusters)}
+	
 	annotation.top.clusters <- cbind(cluster.vec, annotation.top.clusters)
 	colnames(annotation.top.clusters)[1] <- "cluster"
 	
-	#### annotation.top.clusters is one of the require output. 
+	#### annotation.top.clusters is one of the required outputs. 
 	
-	## finally, inidicidual site analysis of the sites in the top clusters:
+	## finally, inidividual site analysis of the sites in the top clusters:
 	ind.res.mat <- matrix(0, nrow = length(sites.vec), ncol = 2)
 #	colnames(ind.res.mat) <- c("exp_effect", "exp_pval")
 	rownames(ind.res.mat) <- sites.vec
+	
 	
 	model.expr.1.site <- paste("model <- geeglm(temp.meth[ind.comp] ~ exposure[ind.comp] + ")	
 	for (j in 1:ncol(covariates)){
 		 if (j == ncol(covariates)) model.expr.1.site <- paste(model.expr.1.site, colnames(covariates)[j])
 		 	else  	model.expr.1.site <- paste(model.expr.1.site, colnames(covariates)[j], "+")}
-	model.expr.1.site <- paste(model.expr.1.site, ", data = covariates[ind.comp,], id = as.numeric(id)[ind.comp])")
+	model.expr.1.site <- paste(model.expr.1.site, ", data = as.data.frame(covariates[ind.comp,]), id = as.numeric(id)[ind.comp])")
 	
+	if (print.progress) message("starting individual site analysis...")
 	
 	for (i in 1:length(sites.vec)){
 
@@ -92,6 +98,8 @@ function(betas, covariates, exposure, id,
 		
 		ind.res.mat[i,1] <-summary(model)[[6]][2,1]
 		ind.res.mat[i,2] <- summary(model)[[6]][2,4]
+		
+		if (print.progress) print(paste("Analyzed the ", i, "-th site out of ", length(sites.vec), "sites"))
 
 	}
 	ind.res.mat <- data.frame(ind.res.mat)
@@ -99,7 +107,9 @@ function(betas, covariates, exposure, id,
 	colnames(ind.res.mat) <- c("site", "cluster", "exposure effect", "exposure p-values")
 	### This is the third required output.
 	
+	
 	if (!is.null(file.to.print.report)){
+		if (print.progress) message("printing report to file...")
 		
 		sink(file.to.print.report)
 		cat("\\documentclass{article}", "\n")
@@ -117,12 +127,10 @@ function(betas, covariates, exposure, id,
 		sink(file.to.print.report, append = T)
 		cat("\\end{document}", "\n")
 		sink()
-
+		if (print.progress) message("finished preparing latex file with summarizing tables")
 	}
 	
 	top.clusters <- top.clusters[,abs.exp.effect := NULL]
-	return(list(top.clusters = top.clusters, 
-              annotation.top.clusters = annotation.top.clusters, 
-              individual.sites.analysis = ind.res.mat))
+	return(list(top.clusters = top.clusters, annotation.top.clusters = annotation.top.clusters, individual.sites.analysis = ind.res.mat))
 	}
 }
